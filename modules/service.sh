6492da27a664e78ec2b6c6f5e82d9aa93add237c
DespairFactor <matthewalex@outlook.com>
2022-10-13 12:00:18 -0400

    .

--- modules/service.sh
+++ modules/service.sh
@@ -8,30 +8,6 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 
-echo 2 > /sys/devices/system/cpu/cpufreq/policy0/sched_pixel/lcpi_threshold
-echo 44 > /sys/devices/system/cpu/cpufreq/policy0/sched_pixel/spc_threshold
-echo 1401000 > /sys/devices/system/cpu/cpufreq/policy0/sched_pixel/limit_frequency
-
-echo 6 > /sys/devices/system/cpu/cpufreq/policy4/sched_pixel/lcpi_threshold
-echo 30> /sys/devices/system/cpu/cpufreq/policy4/sched_pixel/spc_threshold
-echo 1826000 > /sys/devices/system/cpu/cpufreq/policy4/sched_pixel/limit_frequency
-
-echo 5 > /sys/devices/system/cpu/cpufreq/policy6/sched_pixel/lcpi_threshold
-echo 40 > /sys/devices/system/cpu/cpufreq/policy6/sched_pixel/spc_threshold
-echo 2401000 > /sys/devices/system/cpu/cpufreq/policy6/sched_pixel/limit_frequency
-
-echo 10 > /sys/kernel/vendor_sched/pmu_poll_time
-echo 1 > /sys/kernel/vendor_sched/pmu_poll_enable
-echo 1 > /sys/devices/system/cpu/cpufreq/policy0/sched_pixel/pmu_limit_enable
-echo 1 > /sys/devices/system/cpu/cpufreq/policy4/sched_pixel/pmu_limit_enable
-echo 1 > /sys/devices/system/cpu/cpufreq/policy6/sched_pixel/pmu_limit_enable
-
-sleep 30;
-
-
-magiskpolicy --live "allow hal_power_default proc file { write read open getattr map }"
-magiskpolicy --live "allow hal_power_default proc_sched file { write read open getattr map }"
-
-stop vendor.power-hal-aidl;
-
-start vendor.power-hal-aidl;
+echo "262144 524288 8388608" /proc/sys/net/ipv4/tcp_wmem
+echo y > /sys/kernel/mm/lru_gen/enabled
+echo 5000 > /sys/kernel/mm/lru_gen/min_ttl_ms
